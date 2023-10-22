9aa0cbf663ede363ea710e710118b526aed329e0
rishxbh <rishabh112000@gmail.com>
2023-10-22 16:12:59 +0530

    footerChange

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './footer.css'
 const Footer = () => {
   return (
-    <footer className="footer">Copyright &#169; 2023 Rishabh Ritika Mehta. All right received.</footer>
+    <footer className="footer">Copyright &#169; 2023 Rishabh Mehta. All right received.</footer>
   )
 }
 
